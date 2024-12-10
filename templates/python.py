04060f91e0055086738084d82dfbac4b46f5cbb7
DyBev <54762882+DyBev@users.noreply.github.com>
2024-12-10 21:42:52 +0000

    feat(python): added python template file

---
+++ templates/python.py
@@ -0,0 +1,17 @@
+day: str = "01"
+filepath: str = "{}/input.txt".format(day)
+
+with open("./{}".format(filepath), "r") as file:
+    fileData = file.readlines()
+
+def part1() -> int:
+    answer: int = 0
+    return answer
+
+def part2() -> int:
+    answer: int = 0
+    return answer
+
+print("Part 1 answer: {}".format(part1))
+print("Part 2 answer: {}".format(part2))
+
